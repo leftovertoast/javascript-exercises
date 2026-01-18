bda7c4a67e0796c548753ad4b319eb005a03dd72
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-18 18:53:12 -0500

    add function to remove elements from given array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...selectedToRemove) {
+    const newArray = [];
+
+    givenArray.forEach((item) => {
+        
+        if (!selectedToRemove.includes(item)) {
+            newArray.push(item);
+        }
+     });
+     return newArray;
 };
 
 // Do not edit below this line
