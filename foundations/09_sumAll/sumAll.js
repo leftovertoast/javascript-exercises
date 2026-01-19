c383d87de7e3b860b7face1dcc9d67f14019bfba
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-18 19:25:08 -0500

    add code to sum all integers between two numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x >= 0 && y >=0 
+        && Number.isInteger(x) 
+        && Number.isInteger(y)) {
+            if (x > y) { 
+            let sum = 0; 
+            for (i = y; y <= x; y++) {
+            sum += y;
+            };
+            return sum;
+            } else if (y > x) {
+            let sum = 0; 
+            for (i = x; x <= y; x++) {
+                sum += x;
+            };
+            return sum;
+            };
+        } else {
+            return "ERROR";
+        };
 };
+    
+    
 
 // Do not edit below this line
 module.exports = sumAll;
