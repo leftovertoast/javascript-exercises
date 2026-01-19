f3cf5e32e888b3b2d754f4e2d9e8fbde22cead14
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-18 21:07:40 -0500

    add code to convert temperatures correctly and round to one decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let conversion = ((fahrenheitTemp - 32) * (5 / 9));
+  return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let conversion = (celsiusTemp * (9 / 5) + 32);
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
