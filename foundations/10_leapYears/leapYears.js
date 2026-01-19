653811c471826a722084341e2881d27d0265322e
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-18 20:41:03 -0500

    add logic to determine if input is leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let isDivBy400 = year % 400 === 0; 
+    let isDivBy100 = year % 100 === 0; 
+    let isDivBy4 = year % 4 === 0;
+
+    if (isDivBy4 && !isDivBy100 || isDivBy400) {
+        return true;
+    } else {
+        return false;
+    };
 
 };
 
