fc6d8cf3d4eebcb4911f98b067d23d65103f674b
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-30 14:13:44 -0500

    complete exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+    let number
+
+    if (typeof num !== 'number') {
+        number = parseInt(num)
+    } else {
+        number = num;
+    }
+
+    if (number < 0) {
+        return "OOPS";
+    };
+
+    if (number == 0) {
+        return 0;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (i = 2; i <= number; i++) { 
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
+}; 
 
 // Do not edit below this line
 module.exports = fibonacci;
