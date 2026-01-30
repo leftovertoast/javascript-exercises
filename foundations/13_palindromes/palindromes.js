7d86b3413b4220aaa46b1e1b1cbe93e259b2b7e6
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-30 13:25:06 -0500

    complete palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (someString) {
+    const cleanChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const newString = someString
+    .toLowerCase()
+    .split('')
+    .filter((chars) => cleanChars.includes(chars))
+    .join('');
 
+    const reversedString = newString.split('').reverse().join('');
+
+    return reversedString === newString;
 };
 
 // Do not edit below this line
