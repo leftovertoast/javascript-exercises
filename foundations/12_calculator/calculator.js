553818f98b1c8a026bfec52a863f85eed5a937e0
cheeks <134818917+leftovertoast@users.noreply.github.com>
2026-01-29 22:18:11 -0500

    complete exercise up to factorials, work needed there

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,53 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a + b);	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(givenArray) {
+  let summedUp = 0
+  for (i = 0; i < givenArray.length; i++) {
+      summedUp += givenArray[i];
+  }
+    return summedUp;
 };
 
-const multiply = function() {
-
+const multiply = function(givenArray) {
+    let product = null
+    for (i = 0; i < givenArray.length; i++) {
+      if (product === null) {
+        product = givenArray[i];
+      } else {
+      product = product * givenArray[i];
+    }; 
+  };
+    return product;
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+  let power = b
+      for (i = 1; i < e; i++) {
+        power = power * b;
+      };
+      return power;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+    factor = null
+    if (num <= 1) {
+      return 1;
+    } else if (num === 2) {
+      return 2;
+    } else if (factor === null) {
+      factor = num;
+    } else for (i = num; i > 0;  i--) {
+        factor = factor * num;
+        };
+    return factor;
+    };
+  
 
 // Do not edit below this line
 module.exports = {
